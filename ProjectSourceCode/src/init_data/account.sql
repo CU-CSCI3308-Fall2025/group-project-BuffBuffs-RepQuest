30f3d6d353477347d20f353c601a44b765cd3034
Addie Hoeft <alhoeft6@gmail.com>
2025-11-01 13:30:15 -0600

    fake data

---
+++ ProjectSourceCode/src/init_data/account.sql
@@ -0,0 +1,8 @@
+INSERT INTO users
+  (username, name, password)
+VALUES
+    (user1, Bob, abcd1),
+    (user2, Steve, abcd2),
+    (user3, John, abcd3),
+    (user4, Sally, abcd4),
+    (user5, Jane, abcd5);
