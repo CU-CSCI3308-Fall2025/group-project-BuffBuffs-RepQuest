a6f1f32bdf234a6c170caa2844cf6fa0da25ad13
Keira Wagstaff <keirawagstaff@gmail.com>
2025-11-04 12:51:18 -0700

    Skeleton for nav and header

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -74,4 +74,5 @@ app.post('/register', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
+
