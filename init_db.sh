c10c2561ccd10612c0be0bee661103dee96a1454
Dalton Ford <dalton.ford77@gmail.com>
2025-11-23 14:37:20 -0700

    updated readme and fixed bugs

---
+++ init_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# DO NOT PUSH THIS FILE TO GITHUB
+# This file contains sensitive information and should be kept private
+
+# TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
+PG_URI="postgresql://users_db_8gyr_user:5avJX011heXz0FUzQVzRBKFvSro2Jaa3@dpg-d4hlssh5pdvs739ase5g-a.oregon-postgres.render.com/users_db_8gyr"
+
+# Execute each .sql file in the directory
+for file in ProjectSourceCode/src/init_data/*.sql; do
+    echo "Executing $file..."
+    psql "$PG_URI" -f "$file"
+done
